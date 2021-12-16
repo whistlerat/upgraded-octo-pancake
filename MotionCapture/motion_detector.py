d8270f4f47ae3278e1bc51194cc106089e909c5e
Raquel Lin <rlin016@ucr.edu>
2021-12-16 02:51:44 -0800

    Finished part 1 of motion capture tutorial

--- MotionCapture/motion_detector.py
+++ MotionCapture/motion_detector.py
@@ -8,8 +8,7 @@ import cv2
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", help="path to the video file")
-ap.add_argument("-a", "--min-area", type=int,
-                default=500, help="minimum area size")
+ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
 args = vars(ap.parse_args())
 
 # if the video argument is None, then we are reading from the webcam
@@ -51,13 +50,14 @@ while True:
         continue
 
     # Compute the absolute difference between the current frame and first frame
+    #### NEED TO SET THE THRESHOLD TO THE ENVIRONMENT
     frameDelta = cv2.absdiff(firstFrame, gray)
     thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
 
     # dilate the thresholded image to fill in holes, then find contours
     # on the thresholded image
     thresh = cv2.dilate(thresh, None, iterations=2)
-    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL), cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
 
     # loop over the contours
@@ -68,4 +68,38 @@ while True:
 
         # compute the bounding box for the contour, draw it on the frame,
         # and update the text
+        (x, y, w, h) = cv2.boundingRect(c)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        text = "Occupied"
+    cv2.putText(
+        frame,
+        "Room Status: {}".format(text),
+        (10, 20),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        0.5,
+        (0, 0, 255),
+        2,
+    )
+    cv2.putText(
+        frame,
+        datetime.datetime.now().strftime("%A %d %B %Y %I: %M %S %p"),
+        (10, frame.shape[0] - 10),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        0.35,
+        (0, 0, 255),
+        1,
+    )
 
+    # show the frame and record if the user presses a key
+    cv2.imshow("Security Feed", frame)
+    cv2.imshow("Thresh", thresh)
+    cv2.imshow("Frame Delta", frameDelta)
+    key = cv2.waitKey(1) & 0xFF
+
+    # If the 'q' key is pressed, break from the loop
+
+    if key == ord("q"):
+        break
+
+vs.stop() if args.get("video", None) is None else vs.release()
+cv2.destroyAllWindows()
